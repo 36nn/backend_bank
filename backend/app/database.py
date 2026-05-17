62df23999caf8f841e9abbaa7c1af3c2195e194d
online-edu-mirea-ru <zz09062006zz@gmail.com>
2026-05-17 16:18:27 +0300

    fix backend structure

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = "DATABASE_URL"
+DATABASE_URL = os.getenv("DATABASE_URL")  # Исправлено!
 
 engine = create_engine(DATABASE_URL)
 
